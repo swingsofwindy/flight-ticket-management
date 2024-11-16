d82be998fb37f92f93cb4b7e6528b56358afa9c1
swingsofwindy <22521717@gm.uit.edu.vn>
2024-11-16 21:06:24 +0700

    create repo

---
+++ DEMO/DAL/DatabaseAccess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using DTO;
+using System.Security.Principal;
+
+
+
+namespace DAL
+{
+    public class SqlConnectionData
+    {
+        public static SqlConnection Connect()
+        {
+
+
+            //string strcon = @"Data Source=HUNG;Initial Catalog=airplanedb;Integrated Security=True";    
+            //string strcon = @"Data Source=LAPTOP-978A4PM7;Initial Catalog=airplanedb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+            //string strcon = @"Data Source=SPIDEY;Initial Catalog=airplanedb;Integrated Security=True";
+            //string strcon = @"Data Source=ZALAW;Initial Catalog=airplanedb;Integrated Security=True";
+            //string strcon = @"Data Source=PHUONGVY\SQLEXPRESS;Initial Catalog=airplanedb;Integrated Security=True";
+            string strcon = @"Data Source=172.28.62.1;Initial Catalog=airplanedb;User ID=SA;Password=@Kimyen2004;";
+            SqlConnection conn = new SqlConnection(strcon); // khởi tạo connect
+            return conn;
+        }
+    }
+    public class DatabaseAccess
+    {
+        // Method to open a connection
+        private static SqlConnection OpenConnection()
+        {
+            SqlConnection conn = SqlConnectionData.Connect();
+            conn.Open();
+            return conn;
+        }
+    }
+}
---
+++ DEMO/GUI/Login.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BLL;
+using DTO;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+                textEmail.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textEmail.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void openAdminForm(string mail, List<string> permissions)
+        {
+            ClientSession.Instance.StartSession(mail, permissions);
+            SessionManager.StartSession(mail, mail, permissions);
+
+            // Mở giao diện admin
+            MainWindow f = new MainWindow();
+            f.Show();
+            Window.GetWindow(this).Close();
+        }
+
+        private void openUserForm(string mail, List<string> permissions)
+        {
+            ClientSession.Instance.StartSession(mail, permissions);
+            SessionManager.StartSession(mail, mail, permissions);
+
+            // Mở giao diện người dùng thông thường
+            StaffWindow f = new StaffWindow();
+            f.Show();
+            Window.GetWindow(this).Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            string email = txtEmail.Text;
+            string password = txtPassword.Password;
+
+            ACCOUNT_BLL accountBLL = new ACCOUNT_BLL();
+            int permissionID;
+
+            if (accountBLL.AuthenticateAccount(email, password, out permissionID))
+            {
+                // Tài khoản và mật khẩu đúng
+                switch (permissionID)
+                {
+                    case 1:
+                        // Xử lý khi permissionID = 1 (Ví dụ: mở giao diện admin)
+                        openAdminForm(email, new[] {permissionID.ToString()}.ToList());
+                        break;
+                    case 2:
+                        // Xử lý khi permissionID = 2 (Ví dụ: mở giao diện người dùng thông thường)
+                        openUserForm(email, new[] { permissionID.ToString() }.ToList());
+                        break;
+                    default:
+                        MessageBox.Show("Invalid permission", "Error");
+                        break;
+                }
+            }
+            else
+            {
+                // Tài khoản hoặc mật khẩu không đúng
+                MessageBox.Show("Invalid account", "Error");
+            }
+
+
+        }
+
+    }
+}
